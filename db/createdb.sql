7cf14124634ab6951f44d88ceb91e8178b7746b7
Dmitry Morozov <9984398@gmail.com>
2024-11-07 17:34:35 +0300

    --> db module: start implement simple orm

---
+++ db/createdb.sql
@@ -0,0 +1,8 @@
+create table Users
+(
+    id       INTEGER PRIMARY KEY,
+    username TEXT    NOT NULL,
+    email    TEXT    NOT NULL,
+    age      INTEGER,
+    balance  INTEGER NOT NULL
+);
---
+++ db/run_db_demo.py
@@ -0,0 +1,9 @@
+import db
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
