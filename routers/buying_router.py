b26c7f9cd1f5b1ef559ef070394ad23495a10d41
Dmitry Morozov <9984398@gmail.com>
2024-11-12 19:51:22 +0300

    -> buying_router: Init refactoring

--- routers/buying_router.py
+++ routers/buying_router.py
@@ -1,4 +1,7 @@
+from os import path
+
 from aiogram import Router, F, types
+from aiogram.types import InputFile, FSInputFile
 
 from resources.keyboards import inline_buying_menu_kbd
 from service.buying import get_buying_list
@@ -9,14 +12,20 @@ buying_router = Router()
 @buying_router.message(F.text == 'Buy')
 async def buying(message: types.Message):
     products = await get_buying_list()
-    product_list_message = "Available products:\n"
 
     for product in products:
-        product_list_message += (
-            f"\nTitle: {product['title']}\n"
+        # Prepare the product details message
+        product_details = (
+            f"Title: {product['title']}\n"
             f"Description: {product['description']}\n"
             f"Price: ${product['price']}\n"
-            f"Image: {product['img']}\n"
         )
 
-    await message.answer(product_list_message, reply_markup=inline_buying_menu_kbd())
+        image_path = path.join('assets/images/', product['img'])
+
+        try:
+            await message.answer_photo(photo=FSInputFile(image_path), caption=product_details)
+        except FileNotFoundError:
+            await message.answer(product_details + "\nImage not found.")
+
+    await message.answer("All products listed above.", reply_markup=inline_buying_menu_kbd())
