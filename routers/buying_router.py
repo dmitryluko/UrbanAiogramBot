a57b9c2c5e0b0ce40f3059d64e61b4666713fbe5
Dmitry Morozov <9984398@gmail.com>
2024-11-17 23:59:48 +0300

    --> feat/bot_upgrade: Done  Review : ready

--- routers/buying_router.py
+++ routers/buying_router.py
@@ -1,4 +1,6 @@
 from os import path
+from venv import logger
+
 from aiogram import Router, F, types
 from aiogram.fsm.context import FSMContext
 from aiogram.types import FSInputFile
@@ -27,26 +29,17 @@ async def buying(message: types.Message, state: FSMContext):
             await message.answer_photo(photo=FSInputFile(image_path), caption=product_details)
         else:
             await message.answer(product_details + "\nImage not found.")
-    await message.answer("All products listed above.", reply_markup=inline_buying_menu_kbd())
+    await message.answer('All products listed above.', reply_markup=inline_buying_menu_kbd())
 
 
-@buying_router.message(F.data == 'product_buying')
-async def start_deal(message: types.Message, state: FSMContext):
-    """
-    Handler for starting a deal after listing all products.
-    """
-    await state.set_state(BuyingState.product)
-
-
-@buying_router.message(BuyingState.product)
+@buying_router.callback_query(F.data == 'product_buying')
 async def handle_the_deal(message: types.Message, state: FSMContext):
     """
     Handler for dealing with the product buying process.
     """
     # Retrieve product information from state
-    product_data = await state.get_data()
-    product_details = product_data.get('product_details', 'No product details available.')
+
     await state.clear()
     await message.answer(
-        f"Thank you for your purchase! Your balance has been updated.\n\nProduct Information:\n{product_details}"
+        f'Thank you for your purchase! Your balance has been updated.'
     )
