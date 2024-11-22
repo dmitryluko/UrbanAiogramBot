cc46309b3b7b4a3f5786f4d003489a8024ef6a84
Dmitry Morozov <9984398@gmail.com>
2024-11-22 22:10:08 +1100

    --> create_product_db.sql: Done

---
+++ db/create_product_db.sql
@@ -0,0 +1,8 @@
+create table Products
+(
+    id          INTEGER PRIMARY KEY,
+    title       TEXT    NOT NULL,
+    description TEXT,
+    price       INTEGER NOT NULL
+);
+
--- db/createdb.sql
+++ db/createdb.sql
@@ -6,3 +6,4 @@ create table Users
     age      INTEGER,
     balance  INTEGER NOT NULL
 );
+
